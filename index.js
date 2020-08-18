27c9b7b07f00ffc2a849bfcd72b2d26fbbcecb2b
8903830 <a8903830@8903830s-mbp.home>
2020-08-18 13:40:34 +0100

    added myStory and few small changes

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("entered Js file");
+
+function openForm() {
+  console.log("clicked open form");
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
